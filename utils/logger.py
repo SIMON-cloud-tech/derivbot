b2f3c8ffe65685ac73ba873d974dcb25af9a2086
Simon Mwangangi <simonmbithi143@gmail.com>
2025-09-26 13:32:27 +0300

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,14 @@
+import logging
+
+# Configure the logger
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("bot.log"),
+        logging.StreamHandler()
+    ]
+)
+
+# Export logger for use across modules
+logger = logging.getLogger("DerivBot")
